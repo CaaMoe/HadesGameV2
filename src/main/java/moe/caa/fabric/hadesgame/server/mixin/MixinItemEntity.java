39ff803e28ea72020b3a7b3e16f74324864fe7c5
CaaMoe <Aeva_D@hotmail.com>
2022-05-03 13:29:03 +0800

    掉落物寿命调整

--- src/main/java/moe/caa/fabric/hadesgame/server/mixin/MixinItemEntity.java
+++ src/main/java/moe/caa/fabric/hadesgame/server/mixin/MixinItemEntity.java
@@ -2,19 +2,39 @@ package moe.caa.fabric.hadesgame.server.mixin;
 
 import moe.caa.fabric.hadesgame.server.Contains;
 import moe.caa.fabric.hadesgame.server.IItemEntityHandler;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemEntity.class)
-public abstract class MixinItemEntity implements IItemEntityHandler {
+public abstract class MixinItemEntity extends Entity implements IItemEntityHandler {
     @Shadow
     private int pickupDelay;
+
+    @Shadow private int itemAge;
     private boolean hg_modSpawn = false;
+
+    public MixinItemEntity(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(method = "tick", at = @At("RETURN"))
+    private void onTick(CallbackInfo ci){
+        if(itemAge >= 20 * 10){
+            this.discard();
+        }
+    }
+
 
     @Override
     public void hg_setModSpawn(boolean b) {
